f3a24c5eaf0f4c1ade88df1bdf853a1d15b7212a
Emily <easchoettler@gmail.com>
2022-11-07 09:34:39 -0500

    Update display function to create card divs

--- myScript.js
+++ myScript.js
@@ -1,23 +1,40 @@
 let myLibrary = [];
 let cardContainer = document.querySelector(".card-container");
 
-function Book(author, title, pages, status, rating) {
-    this.author = author;
+function Book(title, author, pages, status, rating) {
     this.title = title;
+    this.author = author;
     this.pages = pages;
     this.status = status;
     this.rating = rating;
 }
 
-function addBookToLibrary(author, title, pages, status, rating) {
-    let bookCopy = new Book(author, title, pages, status, rating)
+function addBookToLibrary(title, author, pages, status, rating) {
+    let bookCopy = new Book(title, author, pages, status, rating)
     myLibrary.push(bookCopy);
 }
 
 function displayBooks(arr) {
     for(let i=0; i < arr.length; i++) {
         let card = document.createElement("div");
-        card.textContent = arr[i].author + arr[i].title + arr[i].pages + arr[i].status + arr[i].rating;
+        let cardTitle = document.createElement("h4");
+        let cardAuthor = document.createElement("p");
+        let cardPages = document.createElement("p");
+        let cardStatus = document.createElement("p");
+        let cardRating = document.createElement("p");
+
+        cardTitle.textContent = "Title: " + arr[i].title;
+        cardAuthor.textContent = "Author: " + arr[i].author;
+        cardPages.textContent = "Pages: " + arr[i].pages;
+        cardStatus.textContent = "Status: " + arr[i].status;
+        cardRating.textContent = "Rating: " + arr[i].rating;
+
+        card.appendChild(cardTitle);
+        card.appendChild(cardAuthor);
+        card.appendChild(cardPages);
+        card.appendChild(cardStatus);
+        card.appendChild(cardRating);
+
         cardContainer.appendChild(card);
     }
 }
